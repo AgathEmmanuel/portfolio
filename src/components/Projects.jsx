f5ae1687f25f1c60234c76e9dccf6296c0938dbe
agathemmanuel <agathemmanuel00@gmail.com>
2024-02-11 16:54:31 +0530

    Adding changes

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -6,8 +6,6 @@ const Projects = () => {
     const data=[
         {name:"MakeYourSpace",description:"A Social Platform to create Relations and Attachments that we create in real world, instead of not just being a networking platform",linkto:"https://github.com/AgathEmmanuel/MakeYourSpace"},
         {name:"RentIt",description:"A Renting Platform where you can rent out anything to anyone in your specified locality and beyond",linkto:"https://github.com/AgathEmmanuel/RentIt"},
-        {name:"vKYCchain",description:"A Vedio KYC Platform based hyperledger fabric for Institution to improve there Business Workflow",linkto:"https://github.com/AgathEmmanuel/vKYCchain"},
-        {name:"CyberHunterz",description:"A Threat Hunting Platform built on top ELK stack using Deep Learning techniques to facilitate continous analysis and detection",linkto:"https://github.com/AgathEmmanuel/CyberHunterz"}
     ];
   return (
      <div>
